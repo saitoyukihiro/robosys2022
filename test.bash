f1cbc1136561dcad7d6759836cb64f3eedf0a765
saitoyukihiro <s.yuki1210@icloud.com>
2022-10-27 13:29:02 +0900

    test.bash

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2022 Yukihiro Saito
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -11,7 +11,16 @@ res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15.0 ]|| ng ${LINENO}
+[ "${out}" = 15 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus) #空文字
+[ "$?" = 1 ]       || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
